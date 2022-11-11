8380a4d03afa2cdadeaead2a1443a75dabde1da7
hieutn1 <hieutn1@vietis.com.vn>
2022-11-11 17:10:53 +0700

    create join room page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:friends_chat/screens/join_room.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -9,15 +10,24 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
+          appBar: AppBar(
+            actions: [
+              IconButton(
+                  onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => JoinRoomScreen(),));
+                  }, icon: Icon(Icons.add))
+            ],
+          ),
           body: Center(
             child: Container(
               child: IconButton(
                 onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => JoinRoomScreen(),));
+                    print('Login Google');
                 },
                 icon: Icon(Icons.g_mobiledata),
               ),
